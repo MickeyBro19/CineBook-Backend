776c438cd5ed5d9b8309c3bdc492c901fba47c84
Mickey <bhaskarp986@gmail.com>
2026-09-15 20:28:15 +0530

    chore: removed redundant access modifiers

--- src/main/java/com/mickey/supportdesk/auth/service/JwtService.java
+++ src/main/java/com/mickey/supportdesk/auth/service/JwtService.java
@@ -7,17 +7,17 @@ public interface JwtService {
 	
 	public String generateToken(User user);
 	
-	public String generateRefreshToken(User user, String jti);
+	String generateRefreshToken(User user, String jti);
 	
-	public boolean isAccessToken(String token);
+	boolean isAccessToken(String token);
 	
-	public Claims parse(String token);
+	Claims parse(String token);
 	
-	public boolean isRefreshToken(String token);
+	boolean isRefreshToken(String token);
 	
-	public Long getUserId(String token);
+	Long getUserId(String token);
 	
-	public Long accessTokenExpiration(String token);
+	Long accessTokenExpiration(String token);
 	
 	Long getAccessTokenTtl();
 	
